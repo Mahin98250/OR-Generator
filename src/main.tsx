94989e56889793d72ab3d7720664424ee618e7e5
Mahin98250 <patelmahin140@gmail.com>
2026-07-16 09:42:45 +0530

    fix: switch to hash router for pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { Toaster } from 'sonner';
 import App from './App';
 import './styles/globals.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
       <Toaster
         richColors
@@ -19,6 +19,6 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
           },
         }}
       />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
