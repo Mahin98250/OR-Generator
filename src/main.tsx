6e0c58061a64eb150b539953cc1766d7b97e7646
Mahin98250 <patelmahin140@gmail.com>
2026-07-17 21:00:27 +0530

    chore: simplify app entry

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
 import { Toaster } from 'sonner';
@@ -6,19 +5,17 @@ import App from './App';
 import './styles/globals.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <HashRouter>
-      <App />
-      <Toaster
-        richColors
-        position="top-right"
-        toastOptions={{
-          style: {
-            borderRadius: '18px',
-            backdropFilter: 'blur(24px)',
-          },
-        }}
-      />
-    </HashRouter>
-  </React.StrictMode>
+  <HashRouter>
+    <App />
+    <Toaster
+      richColors
+      position="top-right"
+      toastOptions={{
+        style: {
+          borderRadius: '18px',
+          backdropFilter: 'blur(24px)',
+        },
+      }}
+    />
+  </HashRouter>
 );
