c1b5e5aa76f80f540d579029d50ce24787e2da00
Mahin98250 <patelmahin140@gmail.com>
2026-09-26 15:06:16 +0530

    ui: constrain app shell for mobile layouts

--- src/components/layout/PageLayout.tsx
+++ src/components/layout/PageLayout.tsx
@@ -4,9 +4,11 @@ import { Navbar } from './Navbar';
 
 export function PageLayout({ children }: { children: ReactNode }) {
   return (
-    <div className="flex min-h-screen flex-col">
+    <div className="app-shell flex min-h-[100dvh] min-w-0 flex-col overflow-x-clip">
       <Navbar />
-      <main className="flex-1 px-4 pb-10 sm:px-6 lg:px-8">{children}</main>
+      <main className="app-main min-w-0 flex-1 px-3 pb-24 pt-2 sm:px-5 sm:pb-10 md:px-6 md:pt-4 lg:px-8">
+        <div className="min-w-0">{children}</div>
+      </main>
       <Footer />
     </div>
   );
